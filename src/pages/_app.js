a4ca24878e354ffe4c838b53ef5c81c5dc515bfe
Faisal <officialfaisal1112@gmail.com>
2023-09-22 16:32:47 +0600

    create header & footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { Footer } from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@material-tailwind/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <div className="max-w-7xl mx-auto">
+        <Header></Header>
+        <div className="min-h-screen">
+          <Component {...pageProps} />
+        </div>
+        <Footer></Footer>
+      </div>
+    </ThemeProvider>
+  );
 }
