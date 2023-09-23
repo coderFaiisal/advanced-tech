cb3e486118479ca8fbce152a51619174596b9a9a
Faisal <officialfaisal1112@gmail.com>
2023-09-23 23:28:15 +0600

    implement nextauth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import RootLayout from "@/components/layout/RootLayout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <RootLayout>
-        <Component {...pageProps} />
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </RootLayout>
     </ThemeProvider>
   );
